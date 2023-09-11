2be4a68b65e822d949ea90d6bccfea636b2c465a
heghine <heghine.balasanyan@gmail.com>
2023-09-11 22:36:42 +0400

    upd

---
+++ CodeRepublic/Week2/gcd.cpp
@@ -0,0 +1,33 @@
+/* PROBLEM: Implement a recursive
+function that calculates the greatest
+common divisor (GCD) of two numbers. */
+
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (a == 0 || b == 0)
+        return (0);
+    else if (a == b)
+        return (a);
+    else if (a > b)
+        return (gcd(a - b, b));
+    else
+        return (gcd(a, b - a));
+}
+
+int main()
+{
+    int num1 = 0, num2 = 0;
+
+    cout << "Enter two positive numbers: ";
+    cin >> num1 >> num2;
+    if (num1 < 0 || num2 < 0)
+    {
+        cout << "Wrong input!\n";
+        exit(1);
+    }
+    cout << "The greatest common divisor of this two numbers is: "
+    << gcd(num1, num2) << endl;
+}
