dc44ce32d89c3f3c6f97aaca384e76a93b332aa8
heghine <heghine.balasanyan@gmail.com>
2023-09-12 18:27:13 +0400

    upd

--- CodeRepublic/Level0/Week2/binary_search.cpp
+++ CodeRepublic/Level0/Week2/binary_search.cpp
@@ -8,18 +8,22 @@ using namespace std;
 int binary_search(int arr[], int target, int start, int end)
 {
     int mid = (start + end) / 2;
-    if (arr[mid] == target)
-        return (mid);
-    else if (arr[mid] < target)
-        start = mid + 1;
-    else
-        end = mid - 1;
-    return (binary_search(arr, target, start, end));
+    if (start <= end)
+    {
+        if (arr[mid] == target)
+            return (mid);
+        else if (arr[mid] < target)
+            start = mid + 1;
+        else
+            end = mid - 1;
+        return (binary_search(arr, target, start, end));
+    }
+    return (-1);
 }
 
 int main()
 {
     int arr[] = {1, 34, 56, 78, INT_MAX};
     int end = (sizeof(arr) / sizeof(arr[0])) - 1;
-    cout << binary_search(arr, INT_MAX, 0, end) << endl;
+    cout << binary_search(arr, 34, 0, end) << endl;
 }
