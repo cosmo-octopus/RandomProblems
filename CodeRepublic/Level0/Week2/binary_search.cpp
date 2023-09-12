41be50d43d1c6f93b2bbc3e90e3c10bfa1e79824
heghine <heghine.balasanyan@gmail.com>
2023-09-12 18:18:50 +0400

    upd

---
+++ CodeRepublic/Level0/Week2/binary_search.cpp
@@ -0,0 +1,25 @@
+/* PROBLEM: Implement binary search
+algorithm recursively */
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int binary_search(int arr[], int target, int start, int end)
+{
+    int mid = (start + end) / 2;
+    if (arr[mid] == target)
+        return (mid);
+    else if (arr[mid] < target)
+        start = mid + 1;
+    else
+        end = mid - 1;
+    return (binary_search(arr, target, start, end));
+}
+
+int main()
+{
+    int arr[] = {1, 34, 56, 78, INT_MAX};
+    int end = (sizeof(arr) / sizeof(arr[0])) - 1;
+    cout << binary_search(arr, INT_MAX, 0, end) << endl;
+}
