f76277c6210db9cfb2b88e93e835222fc5493530
heghine <heghine.balasanyan@gmail.com>
2023-09-20 02:47:53 +0400

    upd

---
+++ CodeRepublic/Level0/Week3/Pointers/strchr.cpp
@@ -0,0 +1,22 @@
+/* PROBLEM: Implement <cstring> strchr recursively.
+(Returns a pointer to the first occurrence
+of character in the C string str.) */
+
+#include <iostream>
+using namespace std;
+
+const char    *ft_strchr(const char *str, const char c)
+{
+
+    if (*str && *(str) != c)
+        return (ft_strchr(++str, c));
+    return (str);
+}
+
+int main()
+{
+    const char  *str = "Hello you, traveler!";
+    const char  c = ',';
+    cout << (void*)ft_strchr(str, c) << '\n';
+    cout << *(ft_strchr(str, c)) << endl;
+}
