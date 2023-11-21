93b25940b4dbb10585869988e1bfd06d32443229
heghine <heghine.balasanyan@gmail.com>
2023-11-21 23:30:07 +0400

    upd

--- CodeRepublic/Level-01/Vector/Vector.hpp
+++ CodeRepublic/Level-01/Vector/Vector.hpp
@@ -1,7 +1,12 @@
 #ifndef __VECTOR_HPP__
 # define __VECTOR_HPP__
 
+/***************************************************************/
+
 # include <iostream>
+# include <limits>
+
+/***************************************************************/
 
 template <typename T>
 class	Vector
@@ -14,11 +19,18 @@ class	Vector
     	Vector(void);
    		~Vector();
 
-		void	vec_print(void) const;
 		size_t	get_size() const;
 		size_t	get_capacity() const;
+		void	vec_print(void) const;
 		void	push_back(const T &elem);
+		size_t	max_size(void) const;
+		void	reserve(size_t capacity);
+		void	resize(size_t size);
+		bool	empty(void) const;
+		void	shrink_to_fit(void);
 };
+
+/***************************************************************/
 
 template <typename T>
 Vector<T>::Vector(void): array(nullptr), capacity(0), size(0)
@@ -46,7 +58,7 @@ size_t	Vector<T>::get_capacity() const
 template <typename T>
 void	Vector<T>::push_back(const T &elem)
 {
-    T	*new_array;
+    T	*array;
 
 	if (size >= capacity)
 	{
@@ -54,13 +66,15 @@ void	Vector<T>::push_back(const T &elem)
             capacity = 1;
         else
             capacity *= 2;
-		new_array = new T[capacity];
+        array = new T[capacity];
         for (size_t i = 0; i < size; i++)
-            new_array[i] = this->array[i];
-        delete[] array;
-        this->array = new_array;
+            array[i] = this->array[i];
+        delete[] this->array;
+        this->array = array;
         this->array[size++] = elem;
 	}
+    else
+        this->array[size++] = elem;
 }
 
 template <typename T>
@@ -69,6 +83,75 @@ void	Vector<T>::vec_print(void) const
 	for (size_t i = 0; i < this->size; i++)
 		std::cout << this->array[i] << " ";
 	std::cout << std::endl;
+}
+
+template <typename T>
+size_t	Vector<T>::max_size(void) const
+{
+	return (std::numeric_limits<size_t>::max());
+}
+
+template <typename T>
+void	Vector<T>::reserve(size_t capacity)
+{
+	T	*arr;
+
+	if (capacity > this->capacity)
+	{
+		arr = new T[capacity];
+		for (size_t i = 0; i < this->size; i++)
+			arr[i] = this->array[i];
+		delete[] this->array;
+		this->array = arr;
+		this->capacity = capacity;
+	}
+}
+
+template <typename T>
+void	Vector<T>::resize(size_t size)
+{
+	if (size < this->size)
+		this->size = size;
+	else if (size > this->size)
+	{
+		reserve(size);
+		while (this->size < size)
+		{
+			this->array[this->size] = T();
+			this->size++;
+		}
+	}
+
+}
+
+template <typename T>
+bool	Vector<T>::empty(void) const
+{
+	return (this->size == 0);
+}
+
+template <typename T>
+void	Vector<T>::shrink_to_fit(void)
+{
+	T	*array;
+	
+	if (this->size < this->capacity)
+	{
+		if (size == 0)
+		{
+			delete[] this->array;
+			this->array = nullptr;
+		}
+		else
+		{
+			array = new T[this->size];
+			for (size_t i = 0; i < this->size; i++)
+				array[i] = this->array[i];
+			delete[] this->array;
+			this->array = array;
+		}
+		capacity = size;
+	}
 }
 
 #endif
--- CodeRepublic/Level-01/Vector/main.cpp
+++ CodeRepublic/Level-01/Vector/main.cpp
@@ -4,7 +4,16 @@ int	main()
 {
 	Vector<int>	vec;
 
-	vec.push_back(10);
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
+	std::cout << vec.get_size() << " " << vec.get_capacity() << std::endl;
+
 	vec.vec_print();
+	vec.resize(2);
+	vec.vec_print();
+	std::cout << vec.get_size() << " " << vec.get_capacity() << std::endl;
+	vec.shrink_to_fit();
 	std::cout << vec.get_size() << " " << vec.get_capacity() << std::endl;
 }
