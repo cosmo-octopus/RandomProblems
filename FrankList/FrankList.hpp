82b7144c9ca364c78ff7c46df38359b96410e36a
heghine <heghine.balasanyan@gmail.com>
2024-03-08 18:06:13 +0400

    franklist upd

--- FrankList/FrankList.hpp
+++ FrankList/FrankList.hpp
@@ -18,13 +18,6 @@ std::ostream	&operator<<(std::ostream &os, FrankList<T> &rhv)
 	return (os);
 }
 
-// using value_type = T;
-// using reference = value_type&;
-// using const_reference = const value_type&;
-// using size_type = std::size_t;
-// using pointer = value_type*;
-// using const_pointer = const value_type*;
-
 /* Node ctors */
 
 template <typename T>
@@ -122,6 +115,180 @@ template <typename T>
 FrankList<T>::~FrankList()
 {
 	clear();
+}
+
+template <typename T>
+void	FrankList<T>::swap(FrankList<value_type> &rhv)
+{
+	std::swap(this->head, rhv.head);
+	std::swap(this->tail, rhv.tail);
+	std::swap(this->ahead, rhv.ahead);
+	std::swap(this->atail, rhv.atail);
+}
+
+template <typename T>
+typename FrankList<T>::size_type	FrankList<T>::size() const
+{
+	size_type	size = 0;
+
+	for (auto it = cbegin(); it != cend(); ++it)
+		++size;
+	return (size);
+}
+
+template <typename T>
+bool	FrankList<T>::empty() const
+{
+	return (size() == 0 ? true : false);
+}
+
+template <typename T>
+void	FrankList<T>::resize(size_type s, const_reference init = value_type())
+{
+	size_type	size = size();
+
+	if (size > s)
+	{
+		for (size_type i = 0; i < size - s; i++)
+			pop_back();
+	}
+	else
+	{
+		for (size_type i = 0; i < s - size; i++)
+			push_back(init);
+	}
+}
+
+template <typename T>
+void	FrankList<T>::clear() noexcept
+{
+	Node	*tmp = head;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		delete tmp;
+	}
+	this->head = nullptr;
+	this->tail = nullptr;
+	this->ahead = nullptr;
+	this->atail = nullptr;
+}
+
+/* to insert a node (ptr) into a doubly linked list in
+ascending order based on the val member of each node */
+template <typename T>
+void	FrankList<T>::put_in_sorted_order(Node *ptr)
+{
+	assert(ptr != nullptr);
+
+	if (!ahead)
+	{
+		ahead = ptr;
+		atail = ahead;
+		return ;
+	}
+
+	Node	*curr = ahead;
+
+	while (curr->val <= ptr->val && curr->asc)
+		curr = curr->asc;
+
+	if (curr->val < ptr->val)
+	{
+		ptr->asc = curr->asc;
+		curr->asc = ptr;
+		ptr->desc = curr;
+		if (!ptr->asc)
+			atail = ptr;
+	}
+	else
+	{
+		ptr->desc = curr->desc;
+		if (curr->desc)
+			curr->desc->asc = ptr;
+		else
+			ahead = ptr;
+		ptr->asc = curr;
+		curr->desc = ptr;
+	}
+}
+
+template <typename T>
+void	FrankList<T>::push_front(const_reference elem)
+{
+	Node	*node = new Node(elem);
+
+	if (!head)
+	{
+		this->head = node;
+		this->tail = head;
+	}
+	else
+	{
+		node->next = head;
+		head->prev = node;
+		head = node;
+	}
+	put_in_sorted_order(head);
+}
+
+template <typename T>
+void	FrankList<T>::pop_front()
+{
+	assert(head != nullptr)
+
+	if (!head->next)
+	{
+		clear();
+		return ;
+	}
+	if (head->asc)
+		head->asc->desc = head->desc;
+	if (head->desc)
+		head->desc->asc = head->asc;
+	head = head->next;
+	delete head->prev;
+	head->prev = nullptr;
+}
+
+template <typename T>
+void	FrankList<T>::push_back(const_reference elem)
+{
+	Node	*node = new Node(elem);
+
+	if (!head)
+	{
+		head = node;
+		tail = head;
+	}
+	else
+	{
+		tail->next = node;
+		node->prev = tail;
+		tail = node;
+	}
+	put_in_sorted_order(tail);
+}
+
+template <typename T>
+void	FrankList<T>::pop_back()
+{
+	assert(head != nullptr);
+
+	if (!head->next)
+	{
+		clear();
+		return ;
+	}
+	if (tail->asc)
+		tail->asc->desc = tail->desc;
+	if (tail->desc)
+		tail->desc->asc = tail->asc;
+	tail = tail->prev;
+	delete tail->next;
+	tail->next = nullptr;
 }
 
 
