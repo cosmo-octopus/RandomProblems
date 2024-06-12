4fb88d5b646fd0ba3358538e040b1931b23a75c8
heghine <heghine.balasanyan@gmail.com>
2024-06-12 23:45:42 +0400

    implementation ++

--- Algorithms/sort_selection.cpp
+++ Algorithms/sort_selection.cpp
@@ -1,57 +1,34 @@
-/***************************
-	Best Case: O(n^2)
-	Average Case: O(n^2)
-	Worst Case: O(n^2)
-***************************/
+/****************************/
+/*	Best Case: O(n^2)		*/
+/*	Average Case: O(n^2)	*/
+/*	Worst Case: O(n^2)		*/
+/****************************/
 
 #include <iostream>
 #include <vector>
 
 void	sort_selection(std::vector<int>& vec)
 {
-	for (auto it1 = vec.begin(); it1 != vec.end(); ++it1)
+	for (size_t i = 0; i < vec.size(); ++i)
 	{
-		auto key = it1;
-		for (auto it2 = it1 + 1; it2 != vec.end(); ++it2)
+		size_t min_idx = i;
+		size_t idx = i;
+		while (idx < vec.size())
 		{
-			if (*it2 < *key)
-				key = it2;
+			if (vec[min_idx] > vec[idx])
+				min_idx = idx;
+			idx ++;
 		}
-		if (key != it1)
-			std::swap(*it1, *key);
+		std::swap(vec[i], vec[min_idx]);
 	}
 }
-
-/*
-using iterator = std::vector<int>::iterator;
-
-void	sort_selection_rec(std::vector<int>&vec, iterator it1, iterator it2, iterator key)
-{
-	if (it1 == vec.end())
-		return ;
-
-	if (it2 != vec.end())
-	{
-		key = *key > *it2 ? it2 : key;
-		++it2;
-	}
-	else
-	{
-		std::swap(*it1, *key);
-		++it1;
-		key = it1;
-		it2 = it1 + 1;
-	}
-	sort_selection_rec(vec, it1, it2, key);
-}
-*/
 
 int	main()
 {
 	std::vector<int>	vec{70, 124, 8, 1, 34, 189, 105, 16, 132, 173, 144, 71, 167, 200, 48};
 
 	sort_selection(vec);
-	// sort_selection_rec(vec, vec.begin(), vec.begin() + 1, vec.begin());
+
 	for (auto it = vec.cbegin(); it != vec.cend(); ++it)
 		std::cout << *it << " ";
 	std::cout << std::endl;
